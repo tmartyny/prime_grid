40b19b7fd01b02bf4d5f4581c5e6a9a70fb6a013
Teresa Martyny <teresamartyny@gmail.com>
2015-07-30 23:04:51 -0700

    initialize number instance variable to start at the first possible prime factor with attr_reader

--- prime_grid.rb
+++ prime_grid.rb
@@ -3,12 +3,12 @@ require_relative 'prime_assessment'
 class PrimeGrid
   include PrimeAssessment
   
-  attr_reader :gridsize, :prime_list
+  attr_reader :gridsize, :prime_list, :number
 
   def initialize(args)
     @gridsize = args.fetch(:gridsize)
     @prime_list = []
-
+    @number = 2
   end
 
   def generate_primes_list
