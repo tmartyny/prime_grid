6ce2b839dbe11766ef5a0649d0a96ab84cb40e68
Teresa Martyny <teresamartyny@gmail.com>
2015-07-30 22:55:50 -0700

    add failing test for generating a list of primes

--- prime_grid_spec.rb
+++ prime_grid_spec.rb
@@ -12,4 +12,9 @@ describe PrimeGrid do
       expect(grid.is_prime?(4)).to be false
     end
   end
+  describe "#generate_primes_list" do
+    it "should generate an array the length of gridsize of prime numbers" do
+      expect(grid.generate_primes_list).to change{grid.prime_list}.from([]).to([2,3,5,7,11,13,17,19,23,29])
+    end
+  end
 end
