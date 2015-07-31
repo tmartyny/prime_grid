b3b3d35400bd5118480bdca5cdca13e3de8db6f6
Teresa Martyny <teresamartyny@gmail.com>
2015-07-30 22:59:19 -0700

    initialize with instance variable and attr_reader for prime_list

--- prime_grid.rb
+++ prime_grid.rb
@@ -3,10 +3,12 @@ require_relative 'prime_assessment'
 class PrimeGrid
   include PrimeAssessment
   
-  attr_reader :gridsize
+  attr_reader :gridsize, :prime_list
 
   def initialize(args)
     @gridsize = args.fetch(:gridsize)
+    @prime_list = []
+
   end
 
   def generate_primes_list
