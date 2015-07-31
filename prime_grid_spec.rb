13518bc278bba8a1854dd590b1b9f179739d9f3b
Teresa Martyny <teresamartyny@gmail.com>
2015-07-30 22:18:26 -0700

    add failing gridsize test

--- prime_grid_spec.rb
+++ prime_grid_spec.rb
@@ -4,4 +4,9 @@ describe PrimeGrid do
   before(:all) do
     grid = PrimeGrid.new(gridsize: 10)
   end
+  describe "#gridsize" do
+    it "should equal 10" do
+      expect(grid.gridsize).to eq(10)
+    end
+  end
 end
